c3060bcb07f591e302998f5ed7025a3969f43a3b
Vincent Bennett <vmbennett99@gmail.com>
2020-09-13 17:46:38 -0400

    removed xmlResponses.js, will combine with jsonResponses

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
-//const xmlHandler = require('./xmlResponses.js');
+// const xmlHandler = require('./xmlResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
