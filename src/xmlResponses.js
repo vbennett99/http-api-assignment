445f4648f0bff671dc459fc751eb2ee6ac78a37e
Vincent Bennett <vmbennett99@gmail.com>
2020-09-13 15:46:51 -0400

    attempting DOMparser fix

--- src/xmlResponses.js
+++ src/xmlResponses.js
@@ -1,7 +1,6 @@
 const respondXML = (request, response, status, object) => {
-    const parser, xmlDoc;
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(object, "text/xml");
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(object, "text/xml");
     response.writeHead(status, {'Content-Type': 'application/xml' });
     response.write(xmlDoc);
     response.end();
