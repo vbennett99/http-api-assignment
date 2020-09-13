86be6676fb9106f168b91aa3963963e041aacf31
Vincent Bennett <vmbennett99@gmail.com>
2020-09-13 15:52:38 -0400

    temp removed xml functionality

--- src/xmlResponses.js
+++ src/xmlResponses.js
@@ -1,8 +1,8 @@
 const respondXML = (request, response, status, object) => {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(object, "text/xml");
+    //const parser = new DOMParser();
+    //const xmlDoc = parser.parseFromString(object, "text/xml");
     response.writeHead(status, {'Content-Type': 'application/xml' });
-    response.write(xmlDoc);
+    response.write(/*xmlDoc*/);
     response.end();
 };
 
