3c0927ac08e56da576b2d42b6a278f8ed3bc89c0
Vincent Bennett <vmbennett99@gmail.com>
2020-09-13 21:41:29 -0400

    JSON responses all work appropriately, only XML not cooperating

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -4,15 +4,25 @@ const respondJSON = (request, response, status, object) => {
   response.end();
 };
 
-const success = (request, response) => {
+const respondXML = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'text/xml' });
+  response.write(object);
+  response.end();
+};
+
+const success = (request, response, type) => {
   const responseJSON = {
     message: 'This is a successful response',
   };
 
-  respondJSON(request, response, 200, responseJSON);
+  if (type === 'text/xml') {
+    const responseXML = `<response><message>${responseJSON.message}</message></response>`;
+    return respondXML(request, response, 200, responseXML);
+  }
+  return respondJSON(request, response, 200, responseJSON);
 };
 
-const badRequest = (request, response, params) => {
+const badRequest = (request, response, type, params) => {
   const responseJSON = {
     message: 'This request has the required parameters',
   };
@@ -20,59 +30,93 @@ const badRequest = (request, response, params) => {
   if (!params.valid || params.valid !== 'true') {
     responseJSON.message = 'Missing valid query parameter set equal to true';
     responseJSON.id = 'badRequest';
+    if (type === 'text/xml') {
+      const responseXML = `<message>${responseJSON.message}</message><id>${responseJSON.id}</id>`;
+      return respondXML(request, response, 400, responseXML);
+    }
     return respondJSON(request, response, 400, responseJSON);
   }
 
+  if (type === 'text/xml') {
+    const responseXML = `<message>${responseJSON.message}</message>`;
+    return respondXML(request, response, 200, responseXML);
+  }
   return respondJSON(request, response, 200, responseJSON);
 };
 
-const unauthorized = (request, response, params) => {
+const unauthorized = (request, response, type, params) => {
   const responseJSON = {
     message: 'This user is authorized',
   };
 
+  // if nessecary param is not present
   if (!params.loggedIn || params.loggedIn !== 'yes') {
-    responseJSON.message = 'This user is unauthorized, please log in';
+    responseJSON.message = 'This user is unauthorized, please set loggedIn value to yes';
     responseJSON.id = 'unauthorized';
-    return respondJSON(request, response, 401, responseJSON);
+    if (type === 'text/xml') {
+      const responseXML = `<message>${responseJSON.message}</message><id>${responseJSON.id}</id>`;
+      return respondXML(request, response, 400, responseXML);
+    }
+    return respondJSON(request, response, 400, responseJSON);
   }
 
+  // if param is present
+  if (type === 'text/xml') {
+    const responseXML = `<message>${responseJSON.message}</message>`;
+    return respondXML(request, response, 200, responseXML);
+  }
   return respondJSON(request, response, 200, responseJSON);
 };
 
-const forbidden = (request, response) => {
+const forbidden = (request, response, type) => {
   const responseJSON = {
     message: 'This user is forbidden from this page',
     id: 'forbidden',
   };
 
-  respondJSON(request, response, 403, responseJSON);
+  if (type === 'text/xml') {
+    const responseXML = `<message>${responseJSON.message}</message><id>${responseJSON.id}</id>`;
+    return respondXML(request, response, 403, responseXML);
+  }
+  return respondJSON(request, response, 403, responseJSON);
 };
 
-const internal = (request, response) => {
+const internal = (request, response, type) => {
   const responseJSON = {
     message: 'Internal error, please try again later',
     id: 'internal',
   };
 
-  respondJSON(request, response, 500, responseJSON);
+  if (type === 'text/xml') {
+    const responseXML = `<message>${responseJSON.message}</message><id>${responseJSON.id}</id>`;
+    return respondXML(request, response, 500, responseXML);
+  }
+  return respondJSON(request, response, 500, responseJSON);
 };
 
-const notImplemented = (request, response) => {
+const notImplemented = (request, response, type) => {
   const responseJSON = {
     message: 'This page is not yet implemented, please try again later',
     id: 'notImplemented',
   };
 
-  respondJSON(request, response, 501, responseJSON);
+  if (type === 'text/xml') {
+    const responseXML = `<message>${responseJSON.message}</message><id>${responseJSON.id}</id>`;
+    return respondXML(request, response, 501, responseXML);
+  }
+  return respondJSON(request, response, 501, responseJSON);
 };
 
-const notFound = (request, response) => {
+const notFound = (request, response, type) => {
   const responseJSON = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
   };
 
+  if (type === 'text/xml') {
+    const responseXML = `<message>${responseJSON.message}</message><id>${responseJSON.id}</id>`;
+    return respondXML(request, response, 404, responseXML);
+  }
   return respondJSON(request, response, 404, responseJSON);
 };
 
--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
-// const xmlHandler = require('./xmlResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -19,14 +18,14 @@ const urlStruct = {
   '/css': htmlHandler.getCSS,
 };
 
-const onRequest = (request, response) => {
+const onRequest = (request, response, type) => {
   const parsedUrl = url.parse(request.url);
   const params = query.parse(parsedUrl.query);
 
   if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, params);
+    urlStruct[parsedUrl.pathname](request, response, type, params);
   } else {
-    urlStruct.notFound(request, response, params);
+    urlStruct.notFound(request, response, type, params);
   }
 };
 
